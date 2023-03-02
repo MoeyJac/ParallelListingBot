5910e4b2ff59f14da4439127d96bb31ca8bf16a5
Moeyjac <jo3ymac@gmail.com>
2023-03-02 01:20:11 -0500

    Added functionality to query Defined API and display card data to user

--- bot.py
+++ bot.py
@@ -1,14 +1,69 @@
 import os
 import discord
 
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
+
+global CLIENT
+
 bot = discord.Bot()
 
+#Bot commands
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 @bot.slash_command(guild_ids=[os.getenv("GUILD_ID")])
 async def hello(ctx):
-    await ctx.respond("Hello Guy!")
+    card_data = await get_card_data()
+    await ctx.respond(card_data)
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+
+async def get_card_data(tokenId=10822):
+    global CLIENT
+
+    query = format_query()
+
+    params = {"tokenId": tokenId}
+    result = await CLIENT.execute_async(query, variable_values=params)
+
+    return format_result(result)
+
+def format_query():
+    return gql(
+        """
+        query ($tokenId: String!){
+            getParallelAssets(tokenIds: [$tokenId]) {
+                items {
+                    gameData {
+                        attack
+                        cost
+                        health
+                    }
+                    name
+                    }
+                }
+            }
+        """
+    )
+
+def format_result(result):
+    name = result["getParallelAssets"]["items"][0]["name"]
+    cost = result["getParallelAssets"]["items"][0]["gameData"]["cost"]
+    attack = result["getParallelAssets"]["items"][0]["gameData"]["attack"]
+    health = result["getParallelAssets"]["items"][0]["gameData"]["health"]
+
+    return f'''Name: {name}\nCost: {cost}\nAttack: {attack}\nHealth: {health}'''
+
+def main():
+    global CLIENT
+    
+    # Select your transport with a defined url endpoint
+    transport = AIOHTTPTransport(url="https://api.defined.fi", headers={'Content-Type': 'application/json', 'x-api-key': os.getenv("DEFINED_API_KEY")})
+
+    # Create a GraphQL client using the defined transport
+    CLIENT = Client(transport=transport, fetch_schema_from_transport=True)
+
+    bot.run(os.getenv("DISCORD_TOKEN"))
+
+main()
